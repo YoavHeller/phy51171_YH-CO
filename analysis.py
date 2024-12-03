8acf716c2a559a4c681ecbc67f4bcd4cd77a9e07
corgonasi <orgonasicharlotte@gmail.com>
2024-12-03 15:01:45 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -15,7 +15,6 @@ class DataProcessor:
         self.density_overtime = []
 
     def energy(self, m, q):
-        
         for t in range(len(self.time_steps)):
             kinetic_energy = m * np.sum(self.velocities[t] ** 2)/2
             electric_energy = q * np.sum(self.electric_field[t] ** 2)/2
@@ -26,15 +25,47 @@ class DataProcessor:
         for t in range(len(self.time_steps)):
             total_momentum = m * np.sum(self.velocities[t])
             self.momentum.append(total_momentum)
-        return self.momentum
+        return momentum
 
-    def density_profile(self, grid_points):
+    def density_profile(self, num_cells):
         density_profiles = []
         for t in range(len(self.time_steps)):
-            density, _ = np.histogram(self.positions[t], bins=grid_points, range=(0, grid_points))
+            density, _ = np.histogram(self.positions[t], bins=num_cells, range=(0, grid_points))
             density_profiles.append(density)
         self.density_overtime = density_profiles
         return density_profiles
     
+    def analysis(self,simulation,show_density,show_energy, show_momentum):
+        if show_density:
+            density_profile = density_profile(self, simulation.num_cells)
+            plt.figure(figsize=(10, 6))
+            plt.scatter(density_profile, [t] * len(density_profile), label=f"Step {t}")
+            plt.xlabel("density")
+            plt.ylabel("Time Step")
+            plt.title("Density Over Time (20 Particles)")
+            plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')  # Place the legend outside the plot
+            plt.tight_layout()
+            plt.show()
+        if show_energy:
+            energy = energy(self,simulation.m, simulation.q)
+            plt.figure(figsize=(10, 6))
+            plt.scatter(energy, [t] * len(energy), label=f"Step {t}")
+            plt.xlabel("energy")
+            plt.ylabel("Time Step")
+            plt.title("Energy Over Time (20 Particles)")
+            plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')  # Place the legend outside the plot
+            plt.tight_layout()
+            plt.show()
+        if show_momentum:
+            momentum = momentum(self, simulation.m)
+            plt.figure(figsize=(10, 6))
+            plt.scatter(momentum, [t] * len(density_profile), label=f"Step {t}")
+            plt.xlabel("momentum")
+            plt.ylabel("Time Step")
+            plt.title("Momentum Over Time (20 Particles)")
+            plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')  # Place the legend outside the plot
+            plt.tight_layout()
+            plt.show()
+        
 
 
